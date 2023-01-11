5c53facea629d6cc8c39b06c8b8e8046e20936b9
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-01-11 12:26:07 +0100

    Renombrar scripts

---
+++ Scripts/00-Plantilla.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#10/01/2023
+
+#Breve explicación de lo que hace el Script.
--- Scripts/02-PasoPorParametros.sh
+++ Scripts/02-PasoPorParametros.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 11/01/2023
+#11/01/2023
 
 # Script que te dice el número de parámetros introducios y cuales son.
 
