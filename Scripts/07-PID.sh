837313585901c3a85226bd409b90b51b60c42886
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-01-14 16:34:44 +0100

    Añadido nuevas condiciones al PID

--- Scripts/07-PID.sh
+++ Scripts/07-PID.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #13/01/2023
-
 #Script para adivinar el PID del Script que se está ejecutando.
 
 clear
@@ -9,25 +8,34 @@ echo "Empieza el juego!"
 
 number=0
 PID=$$
+validacion=^[0-9]+$
 
 echo "Respuesta: $PID"
-
-echo $randomNumber
 
 while [ $number -ne $PID ]
 do
 	read -p "Introduce el número: " number
 
+	if [[ $number =~ ^[0-9]+$ ]]; then
+
+   		echo "El número introducio es valido"
+
+   	else
+
+   		echo "El número $number no es un valor valido." 
+
+   		exit
+   	fi
+
 	if [ $number -gt $PID ]; then
 
-	 	echo "El PID es más pequeño"
+	 	echo "El PID es más pequeño."
 
 	elif [ $number -lt $PID ]; then
 
-	 	echo "El PID es más grande"
+	 	echo "El PID es más grande."
 	fi
 
 done
 
-echo "Enhorabuena, has adivinado el número"
-
+echo "Enhorabuena, has adivinado el número."
